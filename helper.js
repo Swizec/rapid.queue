be751c4490886c7b66212fc2cdc697aedbd9bf17
Swizec Teller <swizec@swizec.com>
2010-11-16 12:53:48 +0100

    helper should be able to do everything boss shouldn't be forced to do

--- helper.js
+++ helper.js
@@ -5,21 +5,6 @@ var redis = require("redis").createClient(),
     logging = require('./logging'),
     settings = require('./settings');
 
-var query = require('url').parse(req.url, true).query;
-	    var list = query && query['list'];
-	    if (!query || !list){
-		res.writeHead(400, {'Content-Type': 'text; encoding=utf-8'});
-		res.end("No query");
-		return;
-	    }
-	    
-	    if (list == 'tasks') handlers.list_tasks(req, res, query)
-	    else if (list == 'logs') handlers.list_logs(req, res, query)
-	    else {
-		res.writeHead(400);
-		res.end("Unknown list");
-	    }
-
 var handlers = {
 
     list_tasks: function (req, res, query) {
@@ -31,6 +16,8 @@ var handlers = {
 	}else{
 	    queue = 'rapid.queue:'+queue;
 	    redis.llen(queue, function (err, len) {
+		if (len > 500) len = 500;
+
 		redis.lrange(queue, 0, len, function (err, tasks) {
 		    res.writeHead(200);
 		    for (var i=0; i < len; i++) {
@@ -47,6 +34,8 @@ var handlers = {
 
     list_logs: function (req, res) {
 	redis.llen("rapid.queue:logs", function (err, len) {
+	    if (len > 500) len = 500;
+
 	    redis.lrange("rapid.queue:logs", 0, len, function (err, entries) {
 		res.writeHead(200);
 		for (var i=0; i < len; i++) {
@@ -56,4 +45,32 @@ var handlers = {
 	    });
 	});
     }
+}
+
+exports.listen = function (port, host) {
+    port = port || 8125, host = host || "127.0.0.1";
+
+    http.createServer(function (req, res) {
+	var query = require('url').parse(req.url, true).query;
+	var list = query && query['list'];
+	if (!query || !list){
+	    res.writeHead(400, {'Content-Type': 'text; encoding=utf-8'});
+	    res.end("No query");
+	    return;
+	}
+	
+	if (list == 'tasks') handlers.list_tasks(req, res, query)
+	else if (list == 'logs') handlers.list_logs(req, res, query)
+	else {
+	    res.writeHead(400);
+	    res.end("Unknown list");
+	}
+    }).listen(port, host);
+}
+
+if (process.argv[0] == 'node') {
+    if (settings.daemonize) {
+	daemon.start();
+    }
+    exports.listen(process.argv[2], process.argv[3])
 }
