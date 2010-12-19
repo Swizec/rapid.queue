093a54edf011a8c00f90c3918572937b06bcefdf
Swizec Teller <swizec@swizec.com>
2010-12-19 03:16:13 +0100

    inner workers need to handle multiple tasks

--- worker.js
+++ worker.js
@@ -35,7 +35,6 @@ exports.listen = function (queue, worker, callback) {
 	var execute = function (tasks, callback) {
 	    var notify_client = function (task, callback) {
 		console.log("notifying");
-		task.result = result;
 		try {
 		    var url = urllib.parse(task.callback);
 		    var body = JSON.stringify(task);
@@ -67,6 +66,7 @@ exports.listen = function (queue, worker, callback) {
 		callbacks++;
 		notify_client(result, function () {
 		    if (callbacks >= tasks.length) {
+			console.log("going for more");
 			callback();
 		    }
 		});
@@ -93,78 +93,27 @@ exports.listen = function (queue, worker, callback) {
 			  for (; i < tasks.length; i++) {
 			      if (tasks[i]) tmp[i] = tasks[i];
 			  }
-			  if (tmp.length > 0)
+			  if (tmp.length > 0) {
 			      execute(tmp, function () {
 				  recurse();
-			      });
+			      })
+			  }else {
+			      recurse();
+			  }
 			  console.log(tmp);
 			  console.log('\n');
 		      }
 		  });
-	return '';
-
-	redis.lpop("rapid.queue:"+queue, function (err, task) {
-	    if (!err && task) {
-		task = JSON.parse(task+"");
-
-		logging.info("Started task "+task.id);
-
-		var notify_client = function (result) {
-		    task.result = result;
-		    try {
-			var url = urllib.parse(task.callback);
-			var body = JSON.stringify(task);
-
-			var client = http.createClient(url.port || 80, url.hostname);
-			var request = client.request('POST', 
-						     url.pathname || '/',
-						     {'host': url.hostname,
-						      'Content-Length': body.length});
-			request.write(body);
-			request.end();
-			request.on("response", function (response) {
-			    if (response.statusCode != 200) {
-				logging.warning("Client responded with error "+task.id);
-			    }else{
-				logging.info("Served task "+task.id);
-			    }
-			    recurse();
-			});
-		    }catch (e) {
-			logging.warning("Client callback unreachable "+task.id);
-			recurse();
-		    }
-		};
-
-		try {
-		    worker(task, notify_client);
-		}catch (e) {
-		    if (settings.notify_errors) {
-			var mail = require('mail').Mail(settings.mail.smtp);
-			mail.message(settings.mail.message)
-			    .body("Task: "+JSON.stringify(task)+"\n\n\n"+e.message+"\n\n"+e.stack)
-			    .send(function(err) {});
-		    }
-
-		    logging.warning("Error serving task "+task.id);
-		    notify_client("ERROR: Big fail\n\nMessage: "+e.message+"\nStack: "+e.stack);
-		}
-	    }else{
-		setTimeout(recurse, 500);
-	    }
-	});
     }
 
     listener.subscribe("rapid.queue:"+queue+":pub");
     listener.on("message", function (channel, message) {
 	console.log("message!");
 	if (message == 'task!' && !BUSY) {
-	    console.log("DOINGIT!");
 	    inner_worker();
 	}
     });
     listener.on("subscribe", function (channel) {
-	console.log("subscribed");
 	callback = callback || function () {};
 	callback();
     });
