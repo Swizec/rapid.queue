ab57e184f9a98198b0a4466e3f4198c7bde9a1a3
Swizec Teller <swizec@swizec.com>
2010-12-19 01:29:53 +0100

    worker should now be receiving task packages

--- worker.js
+++ worker.js
@@ -32,16 +32,74 @@ exports.listen = function (queue, worker, callback) {
 	    }
 	};
 
-	var bla = [1,2,3,4,5];
-	async.mapSeries(bla,
+	var execute = function (tasks, callback) {
+	    var notify_client = function (task, callback) {
+		console.log("notifying");
+		task.result = result;
+		try {
+		    var url = urllib.parse(task.callback);
+		    var body = JSON.stringify(task);
+		    
+		    var client = http.createClient(url.port || 80, url.hostname);
+		    var request = client.request('POST', 
+			url.pathname || '/',
+			{'host': url.hostname,
+			'Content-Length': body.length});
+		    request.write(body);
+		    request.end();
+		    request.on("response", function (response) {
+			if (response.statusCode != 200) {
+			    logging.warning("Client responded with error "+task.id);
+			}else{
+			    logging.info("Served task "+task.id);
+			}
+			
+			callback(null, 'meow');
+		    });
+		}catch (e) {
+		    logging.warning("Client callback unreachable "+task.id);
+		    callback(null, 'meow');
+		}
+	    };
+
+	    var callbacks = 0;
+	    worker(tasks, function (result) {
+		callbacks++;
+		notify_client(result, function () {
+		    if (callbacks >= tasks.length) {
+			callback();
+		    }
+		});
+	    });
+	}
+
+	async.mapSeries([1,2,3,4,5],
 		  function (bla, callback) {
-		      redis.lpop("rapid.queue:"+queue, callback);
+		      redis.lpop("rapid.queue:"+queue, function (err, task) {
+			  if (task) {
+			      task = JSON.parse(task+"");
+			      
+			      logging.info("Started task "+task.id);
+			  
+			      callback(null, task);
+			  }else{
+			      callback(null, null);
+			  }
+		      });
 		  },
 		  function (err, tasks) {
 		      if (!err) {
-			  console.log(tasks);
+			  var tmp = [], i=0;
+			  for (; i < tasks.length; i++) {
+			      if (tasks[i]) tmp[i] = tasks[i];
+			  }
+			  if (tmp.length > 0)
+			      execute(tmp, function () {
+				  recurse();
+			      });
+			  console.log(tmp);
+			  console.log('\n');
 		      }
-		      recurse();
 		  });
 	return '';
 
@@ -101,6 +159,7 @@ exports.listen = function (queue, worker, callback) {
     listener.on("message", function (channel, message) {
 	console.log("message!");
 	if (message == 'task!' && !BUSY) {
+	    console.log("DOINGIT!");
 	    inner_worker();
 	}
     });
